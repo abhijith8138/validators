0aa27b57adb24f166f4b268dbfbaffeb80563295
abhijith8138 <abhijithkbiju.com>
2023-04-26 00:14:40 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -14,3 +14,17 @@ class studentForm(forms.Form):
     Name=forms.CharField(max_length=100)
     age=forms.IntegerField()
     email=forms.EmailField(max_length=100)
+    re_enter_email=forms.EmailField()
+    botcatcher=forms.CharField(max_length=100,widget=forms.HiddenInput,required=False)
+
+    def clean(self):
+        e=self.cleaned_data['email']
+        r=self.cleaned_data['re_enter_email']
+        if e!=r:
+            raise forms.ValidationError('not matched')
+    
+    def clean_botcatcher(self):
+        bot=self.cleaned_data['botcatcher']
+        if len(bot)>0:
+            raise forms.ValidationError('bot')
+    
